b33b52d3aea4584127db0e0254096d8fc6c74091
Marcelo F. Terenciani <marcelo.figterenciani@gmail.com>
2017-08-17 20:24:52 -0300

    Meu primeiro Hello World em Java!

---
+++ src/br/estacio/programacao/PrimeiraClasse.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.programacao;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class PrimeiraClasse {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 10;
+        
+        if (x == 10)
+            System.out.println("Hello World!!!");
+    }
+    
+}
