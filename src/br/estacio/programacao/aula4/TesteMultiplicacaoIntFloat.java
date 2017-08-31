7944976d37114b6a9b3c104024b091dddb3e7941
Marcelo F. Terenciani <marcelo.figterenciani@gmail.com>
2017-08-30 21:44:32 -0300

    Testando a Multiplicação de Float por Int

---
+++ src/br/estacio/programacao/aula4/TesteMultiplicacaoIntFloat.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.programacao.aula4;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class TesteMultiplicacaoIntFloat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int x = 2;
+        float y = 3.1f;
+
+        float z;
+
+        //Organizar código ALT+SHIFT+F
+        
+        z = x * y;
+        //sout CTRL+SPACE
+        System.out.println(z);
+
+    }
+
+}
