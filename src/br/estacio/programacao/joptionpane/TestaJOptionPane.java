a500a558a90dd9d6b8970f0584684ee4c1c4e7d8
Marcelo F. Terenciani <marcelo.figterenciani@gmail.com>
2017-10-19 19:39:14 -0200

    Iniciando com JOPTIONPANE

---
+++ src/br/estacio/programacao/joptionpane/TestaJOptionPane.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.programacao.joptionpane;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Marcelo
+ */
+/*
+    Programa que le a quantidade de numeros do uruasio
+    em seguida solicita os numeros
+    e imprime na tela usando o JOPtionPane
+ */
+public class TestaJOptionPane {
+
+    public static void main(String args[]) {
+        int quantidadeDeNumeros;
+        int count = 1;
+        double soma = 0;
+        double media = 0;
+        String retorno = JOptionPane.showInputDialog(null, "Digite a quantidade de numeros", "Calculadora de Média", JOptionPane.INFORMATION_MESSAGE);
+
+        quantidadeDeNumeros = Integer.parseInt(retorno);
+
+        while (count <= quantidadeDeNumeros) {
+            retorno = JOptionPane.showInputDialog(null, "Digite o numero", "Calculadora de Média", JOptionPane.INFORMATION_MESSAGE);
+            soma += Double.parseDouble(retorno);
+        }
+
+        if (soma != 0) {
+            media = soma / quantidadeDeNumeros;
+            JOptionPane.showMessageDialog(null, "A média é: " + media);
+        }
+
+    }
+
+}
